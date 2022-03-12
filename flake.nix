899e3d107ae3f0bb57614132885c347e74c40535
hugosenari <hugosenari@gmail.com>
2022-03-12 02:05:26 -0300

    chore: re index espeak

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Nim Espeak NG wrapper, for super easy Voice and Text-To-Speech'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."espeak-master".url = "path:./master";
+  inputs."espeak-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."espeak-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Nim Espeak NG wrapper, for super easy Voice and Text-To-Speech'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-espeak-master.flake = false;
+  inputs.src-espeak-master.owner = "juancarlospaco";
+  inputs.src-espeak-master.ref   = "refs/heads/master";
+  inputs.src-espeak-master.repo  = "nim-espeak";
+  inputs.src-espeak-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-espeak-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-espeak-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
